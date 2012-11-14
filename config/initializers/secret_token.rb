e9f50f548f5b8fd45306f2009f8b5d26b01c7572
Joe Lohr <jlohr@stonemeta.com>
2012-11-14 09:28:18 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '24cddfdbb613a5dbaf4241a85231c4d891e08f9057a08f7b80287df2a20fa891a75a1a2d621327bc3e50a78865c9aec805ce94c15fae28a26434fbb32d20edae'
